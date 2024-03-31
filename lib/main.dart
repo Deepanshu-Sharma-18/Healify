c2de21b635696d544d93de8f1cc63b62c3a915fd
Deepanshu <deepanshu.r.sharma@gmail.com>
2024-03-31 11:48:02 +0530

    blockchain intergrate

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:web3modal_flutter/constants/string_constants.dart';
+import 'package:web3modal_flutter/web3modal_flutter.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -13,113 +19,171 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: LoginMetask(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class LoginMetask extends StatefulWidget {
+  const LoginMetask({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<LoginMetask> createState() => _LoginMetaskState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _LoginMetaskState extends State<LoginMetask> {
+  late ContractFunction addPatient;
+  late ContractFunction addRecord;
+  var chainId = "11155111";
+  var contractAddress = "0x94791C982a6d132D5E547a563aE038243C2e6A9F";
+  late ContractFunction deleteRecord;
+  late ContractFunction getPatientsRecords;
+  late ContractFunction grantAccess;
+  late ContractFunction revokeAccess;
+  final sepoliaChain = W3MChainInfo(
+    chainName: 'Sepolia',
+    chainId: "11155111",
+    namespace: 'eip155:11155111',
+    tokenName: 'ETH',
+    rpcUrl: 'https://rpc.sepolia.org/',
+    blockExplorer: W3MBlockExplorer(
+        name: 'Sepolia Explorer', url: 'https://sepolia.etherscan.io/'),
+  );
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  late ContractAbi _abiCode;
+  late Credentials _cred;
+  late DeployedContract _deployedContract;
+  EthPrivateKey? _privateKey;
+  W3MService? _w3mService;
+  late Web3Client _web3client;
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initialize();
+  }
+
+  Future<void> initialize() async {
+    W3MChainPresets.chains.putIfAbsent(chainId, () => sepoliaChain);
+    _w3mService = W3MService(
+      projectId: '23cba4bca496679312a92ad6a7207a86',
+      metadata: const PairingMetadata(
+        name: 'Web3Modal Flutter Example',
+        description: 'Web3Modal Flutter Example',
+        url: 'https://www.walletconnect.com/',
+        icons: ['https://walletconnect.com/walletconnect-logo.png'],
+        redirect: Redirect(
+          native: 'flutterdapp://', // your own custom scheme
+          universal: 'https://www.walletconnect.com',
+        ),
+      ),
+    );
+    await _w3mService!.init();
+  }
+
+  Future<void> callReadFunction() async {
+    const rpcUrl =
+        'https://sepolia.infura.io/v3/c53a21fae8f64a08ae380e76ecf0c18d';
+
+    const socket =
+        "wss://sepolia.infura.io/ws/v3/c53a21fae8f64a08ae380e76ecf0c18d";
+
+    _privateKey = EthPrivateKey.fromHex(
+        "f27d158ea0c056b026c8805c19e22466bb69914104880a1f07c2f6cfd77f52eb");
+
+    _cred = _privateKey!;
+
+    String abiFile = await rootBundle.loadString('assets/HealthRecords.json');
+
+    var jsonAbi = jsonDecode(abiFile);
+
+    _abiCode =
+        ContractAbi.fromJson(jsonEncode(jsonAbi["abi"]), 'HealthRecords');
+
+    _web3client = Web3Client(
+      rpcUrl,
+      http.Client(),
+    );
+
+    _deployedContract =
+        DeployedContract(_abiCode, EthereumAddress.fromHex(contractAddress));
+
+    addPatient = _deployedContract.function('addPatient');
+    addRecord = _deployedContract.function('addRecord');
+    deleteRecord = _deployedContract.function('deleteRecord');
+    getPatientsRecords = _deployedContract.function('getPatientsRecords');
+    grantAccess = _deployedContract.function('grantAccess');
+    revokeAccess = _deployedContract.function('revokeAccess');
+
+    // await _web3client.sendTransaction(
+    //   _cred,
+    //   Transaction.callContract(
+    //       contract: _deployedContract,
+    //       function: addPatient,
+    //       parameters: ["deepanshu"]),
+    //   chainId: 11155111,
+    // );
+
+    // await _web3client.sendTransaction(
+    //   _cred,
+    //   Transaction.callContract(
+    //       contract: _deployedContract,
+    //       function: addRecord,
+    //       parameters: ["deepanshu", "dc1"]),
+    //   chainId: 11155111,
+    // );
+    // await _web3client.sendTransaction(
+    //   _cred,
+    //   Transaction.callContract(
+    //       contract: _deployedContract,
+    //       function: grantAccess,
+    //       parameters: [
+    //         "deepanshu",
+    //         "dc1",
+    //         EthereumAddress.fromHex(
+    //             "0x94791C982a6d132D5E547a563aE038243C2e6A9F")
+    //       ]),
+    //   chainId: 11155111,
+    // );
+
+    final data = await _web3client.call(
+        contract: _deployedContract,
+        function: getPatientsRecords,
+        params: ["deepanshu", "dc1"]);
+
+    print(data.toString());
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      body: SafeArea(
+        child: _w3mService == null
+            ? Center(child: CircularProgressIndicator())
+            : Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  W3MConnectWalletButton(service: _w3mService!),
+                  const SizedBox(height: 20),
+                  W3MNetworkSelectButton(service: _w3mService!),
+                  const SizedBox(height: 20),
+                  W3MAccountButton(service: _w3mService!),
+                  OutlinedButton(
+                      onPressed: () {
+                        callReadFunction();
+                      },
+                      child: Text("Test call contract"))
+                ],
+              ),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
